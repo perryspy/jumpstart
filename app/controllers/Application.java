148f597ed7e55f6344199223031f15f6a1b37cf8
Patrick Hutchinson <rphutchinson@gmail.com>
2013-03-16 22:05:47 -0400

    added twitter bootstrap

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,10 +5,14 @@ import play.mvc.*;
 
 import views.html.*;
 
+/**
+ * Main controller for the Application since this is a "one page app" that is using AngularJS for the client side MVC
+ * architecture there really shouldn't be much in here.
+ */
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("test"));
     }
   
 }
