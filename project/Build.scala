1ade1a22a0fc6e6211c1caf59054a125efaa17cd
Patrick Hutchinson <rphutchinson@gmail.com>
2013-03-16 21:49:07 -0400

    added jbcrypt dependency.  Added a User model class

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
+    "org.mindrot" % "jbcrypt" % "0.3m",
     javaCore,
     javaJdbc,
     javaEbean
